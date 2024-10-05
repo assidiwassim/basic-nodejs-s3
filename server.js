f09110e8c2f09b7ff560d7a6699d8262a48d8133
Wassim Assidi <assidi.wassim.dev@gmail.com>
2024-10-05 21:03:38 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const mongoose = require("mongoose");
+const Item = require("./models/Item");
+const app = express()
+const port = 3001
+require('dotenv').config()
+console.log(process.env) // remove this after you've confirmed it is working
+
+app.use(express.json({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
+
+// "mongodb://localhost:27018/mydb" => connect from my host directly [use localhost & host-port]
+// "mongodb://mongodb-service:27017/mydb"  => connect from an other container in the same network [use service-name & container-port]
+
+mongoose
+    .connect(process.env.DB_HOST, { useNewUrlParser: true })
+    .then(() => console.log("MongoDB Connected"))
+    .catch((err) => console.log("Error = ", err));
+
+
+app.get('/', (req, res) => {
+    res.send('welcome ... test extra !')
+})
+
+app.get("/items", (req, res) => {
+    Item.find()
+        .then((items) => res.send({ items }))
+        .catch((err) => res.status(500).json({ err }));
+});
+
+app.post("/items/add", (req, res) => {
+    const newItem = new Item({
+        name: req.body.name,
+    });
+
+    newItem
+        .save()
+        .then((item) => res.send({ message: "item saved successfully", item }))
+        .catch((err) => res.status(500).json({ err }));
+});
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`)
+})
